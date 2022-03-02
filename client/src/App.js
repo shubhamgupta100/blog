0d4a630f6777d687562319067d06eb637f184d04
Shubham Gupta <sg350348@gmail.com>
2022-03-02 19:05:57 +0530

    page not found added

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Home from "./components/Home/Home";
 import LoginSignUp from "./components/User/LoginSignUp";
 import NewBlog from "./components/Blog/NewBlog";
 import BlogDetails from "./components/Blog/BlogDetails";
+import NotFound from "./components/layout/NotFound/NotFound";
 function App() {
   return (
     <>
@@ -16,6 +17,7 @@ function App() {
           <Route exact path="/login" element={<LoginSignUp />} />
           <Route exact path="/blog/new" element={<NewBlog />} />
           <Route exact path="/blog/:id" element={<BlogDetails />} />
+          <Route path="*" exact={true} element={<NotFound />} />
         </Routes>
       </Router>
     </>
